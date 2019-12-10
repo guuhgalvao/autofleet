f7f858977edd8529f3ef652cdf68e0e7ba54c999
Gustavo Galvão <gu.afgalvao@gmail.com>
2019-12-09 23:58:28 -0300

    cross-origin

--- src/main/java/com/syscon/autofleet/resources/UserResource.java
+++ src/main/java/com/syscon/autofleet/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.syscon.autofleet.models.ResponseData;
 import com.syscon.autofleet.models.User;
 import com.syscon.autofleet.repository.UserRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path="/api")
 public class UserResource {
--- src/main/java/com/syscon/autofleet/resources/VehicleResource.java
+++ src/main/java/com/syscon/autofleet/resources/VehicleResource.java
@@ -3,6 +3,7 @@ package com.syscon.autofleet.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.syscon.autofleet.models.ResponseData;
 import com.syscon.autofleet.models.Vehicle;
 import com.syscon.autofleet.repository.VehicleRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path="/api")
 public class VehicleResource {
